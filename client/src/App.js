2d670e72fe51edd7d5cb80446ce7f82fdb51763e
swhachey <sw.hachey@gmail.com>
2021-03-26 11:54:08 -0600

    created basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import NavBar from "./components/Nav/nav"
 
 function App() {
   return (
+    <div className="container">
+    <NavBar/>
    <h1>GoFort</h1>
+   </div>
   );
 }
 
---
+++ client/src/components/Nav/nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Nav } from "react-bootstrap"
+
+function NavBar() {
+    return (
+        <Nav variant="tabs" defaultActiveKey="/home" className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <Nav.Item>
+    <Nav.Link href="/home">GoFort</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Places I've Been</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Chat</Nav.Link>
+  </Nav.Item>
+</Nav>
+    )
+}
+
+export default NavBar
