b571e04df329a77260505b3ef67c9ea459ff1d96
swhachey <sw.hachey@gmail.com>
2021-03-29 11:12:23 -0600

    setting up API call for search form

--- client/src/pages/Cities.js
+++ client/src/pages/Cities.js
@@ -2,12 +2,25 @@ import React, {useState, useEffect} from 'react'
 import { Link } from "react-router-dom"
 import { List, ListItem} from "../components/List/list"
 import {Input, TextArea, FormBtn} from "../components/SearchForm/searchform"
+import API from "../utils/API"
 
 
 
 function Cities() {
     const [cities, setCities] = useState([])
     const [formObject, setFormObject] = useState({})
+
+  useEffect(() => {
+    loadCities()
+  }, [])
+
+  function loadCities() {
+    API.getCities()
+      .then(res => 
+        setCities(res.data)
+      )
+      .catch(err => console.log(err));
+  };
 
     function handleInputChange(event) {
     const { name, value } = event.target;
---
+++ client/src/utils/API.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default {
+  getCities: function() {
+    return axios.get("/api/cities");
+  },
+
+  getCity: function(id) {
+    return axios.get("/api/cities/" + id);
+  },
+  
+  deleteCity: function(id) {
+    return axios.delete("/api/cities/" + id);
+  },
+
+  saveCity: function(cityData) {
+    return axios.post("/api/cities", bookData);
+  }
+};
