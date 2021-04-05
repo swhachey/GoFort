44cb44dd2e52d0460c3b35858ac618b36733b141
swhachey <sw.hachey@gmail.com>
2021-04-05 12:57:19 -0600

    deploy

--- client/src/components/Nav/nav.js
+++ client/src/components/Nav/nav.js
@@ -9,19 +9,19 @@ function NavBar() {
  <Nav variant="tabs" defaultActiveKey="/home" className="navbar navbar-expand-lg navbar-dark bg-dark">
    <Link to="/">
   <Nav.Item> 
-    <Nav.Link className="navlink" style={{fontFamily: "Carter One", fontSize: "16px"}}>GoFort</Nav.Link>
+    <Nav.Link href="/" className="navlink" style={{fontFamily: "Carter One", fontSize: "16px"}}>GoFort</Nav.Link>
   </Nav.Item>
   </Link>
 
   <Link to="/traveled">
   <Nav.Item>  
-    <Nav.Link eventKey="link-1" className="navlink">Traveled</Nav.Link>
+    <Nav.Link href="/traveled" eventKey="link-1" className="navlink">Traveled</Nav.Link>
   </Nav.Item>
     </Link>
 
  <Link to="/chat">
   <Nav.Item> 
-    <Nav.Link eventKey="link-2" className="navlink">Chat</Nav.Link>
+    <Nav.Link href="/chat" eventKey="link-2" className="navlink">Chat</Nav.Link>
   </Nav.Item>
     </Link>
   <Nav.Item position = "right">
